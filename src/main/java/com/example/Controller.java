a1ec65cdc78029de3ba2d5fb5b641e72876fb6fa
El Mehdi Talbi <talbimehdi432@gmail.com>
2025-10-29 13:00:36 +0000

    initialize

---
+++ src/main/java/com/example/Controller.java
@@ -0,0 +1,13 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class COntroller {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello, World!";
+    }
+}
